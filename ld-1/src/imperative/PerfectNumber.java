e7a8b4f1c45353312df154e5379f4aba77d019d3
Berzkalne <zaneberzkalne@gmail.com>
2018-09-15 11:36:10 +0300

    Perfect Number problem first part

---
+++ ld-1/src/imperative/PerfectNumber.java
@@ -0,0 +1,35 @@
+package imperative;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class PerfectNumber {
+    public enum STATE { ABUNDANT, DEFICIENT, PERFECT }
+
+    public static STATE detect(int number) {
+        return process(number);
+    }
+
+    public static Set<Integer> divisors(int number) {
+        Set list = new HashSet();
+
+        for (int i = 1; i <= number ; i++) {
+            if (number % i == 0) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    public static STATE process(int number) {
+        int divisor_sum = divisors(number).stream().mapToInt(Integer::intValue).sum() / 2;
+
+        if (divisor_sum == number) {
+            return STATE.PERFECT;
+        } else if (divisor_sum > number) {
+            return STATE.ABUNDANT;
+        } else {
+            return STATE.DEFICIENT;
+        }
+    }
+}
