2b0148bc81201a70d52a68521f4a502e7fcf6367
Berzkalne <zaneberzkalne@gmail.com>
2018-09-23 17:29:35 +0300

    superDigit method

--- ld-2/src/imperative/Main.java
+++ ld-2/src/imperative/Main.java
@@ -3,18 +3,13 @@ package imperative;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(digitSum(2345));
-    }
-
-    static int digitSum(int n){
-        if (Integer.toString(n).length() == 1) {
-            return n;
-        } else {
-            return superDigit(n);
-        }
+        System.out.println(superDigit(11));
+        System.out.println(superDigit(2));
+        System.out.println(superDigit(12345));
+        System.out.println(superDigit(513));
     }
 
     static int superDigit(int x) {
-        return x == 0 ? 0 : x % 10 + superDigit(x/10);
+        return x == 0 ? 0 : x % 10 + superDigit(x / 10);
     }
 }
